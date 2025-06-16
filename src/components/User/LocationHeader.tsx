ad2a7e578c8603d43d3ddc6ca5a1e283501d22cf
saalim-km <saalimkm@gmail.com>
2025-06-16 15:34:46 +0530

    fix: update header styling in LocationHeader component for improved typography

--- src/components/User/LocationHeader.tsx
+++ src/components/User/LocationHeader.tsx
@@ -1,5 +1,4 @@
 import { Button } from "@/components/ui/button";
-import { useTheme } from "@/context/ThemeContext";
 import { useThemeConstants } from "@/utils/theme/theme.utils";
 
 interface HeaderProps {
@@ -12,7 +11,7 @@ export default function LocationHeader({ onOpenModal, selectedLocation }: Header
 
   return (
     <header className={`flex flex-col items-center py-20`}>
-      <h1 className="text-3xl font-bold mb-4">India's Best Wedding Photographers</h1>
+      <h1 className="font-serif tracking-tight text-4xl  mb-4">India's Best Wedding Photographers</h1>
       <Button onClick={onOpenModal} className={`${buttonPrimary} text-white px-4 py-2 rounded-md`}>
         {selectedLocation || "Choose Location"}
       </Button>
