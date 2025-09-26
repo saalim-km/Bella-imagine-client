7541c51a12ebf22fcf00b6b2c1045198a248966a
saalim-km <saalimkm@gmail.com>
2025-09-26 19:27:47 +0530

    fix : update top photographer data showing bug

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import ErrorBoundary from "./components/Error/ErrorBoundary.tsx";
 import { ThemeProvider } from "./context/ThemeContext.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Provider } from "react-redux";
-import { store } from "./store/store.ts";
+import { store } from "./store/store.ts"; 
 import AppLayout from "./App.tsx";
 const queryClient = new QueryClient();
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
